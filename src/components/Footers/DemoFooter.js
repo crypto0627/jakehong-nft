4600fd53a39431d11b9c717ed383ccec1a0bbd93
jake0627a1 <jake0627a1@gmail.com>
2022-09-16 17:38:03 +0800

    web3.0

--- src/components/Footers/DemoFooter.js
+++ src/components/Footers/DemoFooter.js
@@ -57,8 +57,8 @@ function DemoFooter() {
           </nav>
           <div className="credits ml-auto">
             <span className="copyright">
-              © {new Date().getFullYear()}, made with{" "}
-              <i className="fa fa-heart heart" /> by Creative Tim
+              © {new Date().toLocaleString()}, Thanks for
+              <i className="fa fa-heart heart" /> React template
             </span>
           </div>
         </Row>
